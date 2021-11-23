7861b017eb60af451da1467c7103de87444732fd
Senhao JIANG <ben659@hotmail.com>
2021-11-23 14:45:39 +0100

    symétrie axiale qui marche à moitié, le Point à mettre en double depuis la branche Point class

--- src/main/java/MAIN.java
+++ src/main/java/MAIN.java
@@ -19,17 +19,20 @@ public class MAIN {
         FormeGeometrique polygon = new Polygone(new Point(2, 0), new HashSet<>(Arrays.asList(ligne, ligne2, ligne3)));
         System.out.println(polygon.calculerPerimetre());
         Image image = new Image();
-        image.ajoutFormes(cercle);
+
+        image.ajoutFormes(ligne);
         image.ajoutFormes(ellipse);
         //image.ajoutFormes(polygon);
         System.out.println(image);
         Point deplacement = new Point(1,2);
         Point homothetie = new Point(4,5);
-        ligne.translation(deplacement);
+
+        ligne.symetrieAxiale(new Ligne(new Point(8, 6),new Point(4,10)));
         System.out.println(ligne.toString());
+
         cercle.translation(deplacement);
-        ligne.homothetie(3,homothetie);
-        System.out.println(ligne.toString());
+        //ligne.homothetie(3,homothetie);
+      //  System.out.println(ligne.toString());
         System.out.println(cercle.toString());
 
     }
--- src/main/java/objects/Ligne.java
+++ src/main/java/objects/Ligne.java
@@ -9,7 +9,7 @@ public class Ligne extends FormeGeometrique implements Comparable<Ligne> {
     public Ligne(Point depart, Point arrive) {
         this.depart = depart;
         this.arrive = arrive;
-        this.longeur = Math.sqrt(Math.pow(2,(arrive.x - depart.x)) + Math.pow(2, (arrive.y - depart.y)));
+        this.longeur = Math.sqrt(Math.pow(2, (arrive.x - depart.x)) + Math.pow(2, (arrive.y - depart.y)));
     }
 
     @Override
@@ -32,7 +32,7 @@ public class Ligne extends FormeGeometrique implements Comparable<Ligne> {
     public void homothetie(double valeur, Point centre) {
         longeur = longeur * valeur;
         this.depart.x = (int) (this.depart.x * valeur * (this.depart.equals(centre) ? 1 : -1));
-        this.depart.y = (int) (this.depart.y * valeur * (this.depart.equals(centre)  ? 1 : -1));
+        this.depart.y = (int) (this.depart.y * valeur * (this.depart.equals(centre) ? 1 : -1));
         this.arrive.x = (int) (this.arrive.x * valeur * (this.arrive.equals(centre) ? 1 : -1));
         this.arrive.y = (int) (this.arrive.y * valeur * (this.arrive.equals(centre) ? 1 : -1));
     }
@@ -40,10 +40,17 @@ public class Ligne extends FormeGeometrique implements Comparable<Ligne> {
     @Override
     public void symetrieAxiale(Ligne axe) {
         // faire un if si possible pour comparer à droite ou à gauche
-        this.depart.x = axe.depart.x - this.depart.x;
-        this.depart.y = axe.depart.y - this.depart.y;
-        this.arrive.x = axe.arrive.x - this.arrive.x;
-        this.arrive.y = axe.arrive.y - this.arrive.y;
+        if (axe.depart.getX() == axe.arrive.getX()) { // Pour le cas d'un axe perpendiculaire à l'axe des abscisses
+            this.depart.x = (2 * axe.depart.x - this.depart.x);
+            this.arrive.x = (2 * axe.arrive.x - this.arrive.x);
+        } else {
+            double a = (axe.arrive.y - axe.depart.y)/(axe.arrive.x - axe.depart.x);
+            double b = axe.depart.y - axe.depart.x;
+            this.depart.x = (int) ((1-Math.pow(a,2) * this.depart.x + 2 * a * this.depart.y - 2*a*b)/(1+Math.pow(a, 2)));
+            this.depart.y = (int) ((2*a*this.depart.x - (1-Math.pow(a, 2))*this.depart.y+2*b)/(1+Math.pow(a, 2)));
+            this.arrive.x = (int) ((1-Math.pow(a,2) * this.arrive.x + 2 * a * this.arrive.y - 2*a*b)/(1+Math.pow(a, 2)));
+            this.arrive.y = (int) ((2*a*this.arrive.x - (1-Math.pow(a, 2))*this.arrive.y+2*b)/(1+Math.pow(a, 2)));
+        }
     }
 
     @Override
